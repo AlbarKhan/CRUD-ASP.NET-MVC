0ff4e2d27a59b2bfad8ce9369b49503a87002642
Albar Khan <albarkhan60@gmail.com>
2024-07-01 22:56:50 +0530

    Add project files.

---
+++ CRUD/Controllers/EmployeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class EmployeController : Controller
+    {
+        // GET: Employe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUD/Models/EmployeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class EmployeModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password {  get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department {  get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
